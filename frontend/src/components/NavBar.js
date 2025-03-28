fb81c9ec046d740903c75f7ed7332c1408a5f32b
padala-t <padala.t@flipkart.com>
2025-03-28 22:56:40 +0530

    NavBar

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className='w-full bg-[#1b1b1b] text-white h-[70px] flex  px-5 py-2 justify-center items-center'>
+        <div className='w-[90%] '>
+            <h1 className='text-[20px] font-bold'>TnS Swagger</h1>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
