93668c05bfbbb9d277fb26a78e568e029de3bb1d
padala-t <padala.t@flipkart.com>
2025-04-11 01:30:16 +0530

    authorization

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,5 @@
-from fastapi import FastAPI, HTTPException, Request
-import httpx
-import asyncio
-import json
-import re
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from json.decoder import JSONDecodeError
-from database.database import get_db, ProjectInfo
 from pydantic import BaseModel
 from routes.swagger import router as swagger_routes
 from routes.config import router as config_routes
