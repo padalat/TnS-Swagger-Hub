d500cde09dc3f45f96fc2df7b7aac9f59647bfcf
Akshay-Pahuja02 <akshay.pahuja@flipkart.com>
2025-04-16 16:36:18 +0530

    migrate from cra to vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home'
+import Layout from './components/Layout';
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ErrorMessage from './components/ErrorMessage';
+import './App.css'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Layout>
+          <Routes>
+            <Route path="/" element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            } />
+            <Route path='error' element={<ErrorMessage error={"You don't have any access"}/>} />
+          </Routes>
+        </Layout>
+      </AuthProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
